b3a2038e16fbb3fc2dd351869b7ac526c65f968a
artursem <artur.semeniuk@gmail.com>
2021-12-06 19:30:51 +0100

    add company logos

--- src/components/JobItem.js
+++ src/components/JobItem.js
@@ -1,7 +1,30 @@
 import React from "react";
-import IconAccount from "./icons/IconAccount";
 import classes from "./JobItem.module.css";
 import TagButton from "./TagButton";
+
+import accountIcon from "../assets/account.svg";
+import eyecamcoIcon from "../assets/eyecam-co.svg";
+import faceitIcon from "../assets/faceit.svg";
+import insureIcon from "../assets/insure.svg";
+import loopstudiosIcon from "../assets/loop-studios.svg";
+import manageIcon from "../assets/manage.svg";
+import myhomeIcon from "../assets/myhome.svg";
+import photosnapIcon from "../assets/photosnap.svg";
+import shortlyIcon from "../assets/shortly.svg";
+import airfilterIcon from "../assets/the-air-filter-company.svg";
+
+const companyLogos = {
+	j1: photosnapIcon,
+	j2: manageIcon,
+	j3: accountIcon,
+	j4: myhomeIcon,
+	j5: loopstudiosIcon,
+	j6: faceitIcon,
+	j7: shortlyIcon,
+	j8: insureIcon,
+	j9: eyecamcoIcon,
+	j10: airfilterIcon,
+};
 
 const JobItem = ({ content }) => {
 	const itemClasses = content.isFeatured
@@ -12,7 +35,7 @@ const JobItem = ({ content }) => {
 		<li key={content.id} className={itemClasses}>
 			<div className={classes.li__main}>
 				<div className={classes.main__logo}>
-					<IconAccount/>
+					<img src={companyLogos[content.id]} />
 				</div>
 				<div className={classes.main__text}>
 					<div className={classes.text__header}>
@@ -42,9 +65,7 @@ const JobItem = ({ content }) => {
 				<TagButton tagWord={content.role} />
 				<TagButton tagWord={content.level} />
 				{[...content.languages, ...content.tools].map((tag) => {
-					return (
-						<TagButton tagWord={tag} key={tag} />
-					);
+					return <TagButton tagWord={tag} key={tag} />;
 				})}
 			</div>
 		</li>
